a2a6ad5a29cc3fd669c3d7d90bcbcc8f73f58dbb
Isaac Law <hoichunlaw@berkeley.edu>
2020-12-31 14:33:27 +0800

    init

---
+++ src/train_covid_lm.sh
@@ -0,0 +1,7 @@
+python3 run_mlm.py \
+    --model_name_or_path /python/output-mlm-6 \
+    --train_file /python/data/train.txt \
+    --validation_file /python/data/val.txt \
+    --do_train \
+    --do_eval \
+    --output_dir /python/output-mlm
